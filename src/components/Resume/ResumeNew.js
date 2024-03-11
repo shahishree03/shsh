3e2406092c1b50dd1290161dea68a6a65ea33e46
Shahi Shreshth <54995727+shahishree03@users.noreply.github.com>
2024-03-11 21:15:45 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/shahishree03/portfolio/master/src/Assets/shahi_resume-converted.pdf";
+  "https://raw.githubusercontent.com/shahishree03/portfolio/master/src/Assets/Shahi_Shreshth.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
